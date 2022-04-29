aa57a311e81b77c4cd2b9bf9f82f8a7cd5de5fa8
Brandon Alexis <brandonmachado159@gmail.com>
2022-04-29 13:45:02 -0500

    adicion de configuracion para habilitar el envio de datos con formularios

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import path from 'path';
 import morgan from "morgan";
 
 import indexRoutes from './routes/index.routes';
-import roleRoutes from './routes/role.routes';
 
 
 // Start Application
@@ -14,12 +13,13 @@ app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // Middelware
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
 
 // Routes
 app.use('/', indexRoutes);
-app.use('/role', roleRoutes);
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')));
