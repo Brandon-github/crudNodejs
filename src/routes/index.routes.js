31af1f3715fb85e5f244552670e42615cbaaeadb
Brandon Alexis <brandonmachado159@gmail.com>
2022-04-29 09:48:27 -0500

    rutas iniciales de la aplicacion

---
+++ src/routes/index.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import controller from '../controllers/indexController';
+
+const router = Router();
+
+router.get('/', controller.getHomeView);
+router.get('/login', controller.getLoginView);
+router.get('/system', controller.getSystemView)
+
+export default router;
