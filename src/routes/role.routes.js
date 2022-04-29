46a40f98c96cc802c76a76c0e13a76fb60534fc2
Brandon Alexis <brandonmachado159@gmail.com>
2022-04-29 13:47:30 -0500

    eliminacion de rutas de roles para modificacion de aplicacion

--- src/routes/role.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import roleController from '../controllers/roleController';
-
-const router = Router();
-
-router.get('/', roleController.getRoles);
-
-export default router;
