1c6970c9d0d7583ce16529bc6eefb8bff10b8901
Brandon Alexis <brandonmachado159@gmail.com>
2022-04-29 09:44:25 -0500

    adicion de configuraciones para las rutas y archivos estaticos

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 import express from "express";
+import path from 'path';
 import morgan from "morgan";
+
+import indexRoutes from './routes/index.routes';
+import roleRoutes from './routes/role.routes';
+
 
 // Start Application
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Middelware
 app.use(morgan('dev'));
+
+// Routes
+app.use('/', indexRoutes);
+app.use('/role', roleRoutes);
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
 
 export default app;
